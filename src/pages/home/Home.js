79c6483029f9cd0db75ca4257b40c9153fdd337d
TixSpik <luisrhxq@hotmail.com>
2020-02-08 22:07:44 -0600

    breaking things but this actually works

---
+++ src/pages/home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Home.scss'
+import Directory from '../../components/directory/Directory'
+
+export default function Home() {
+    return (
+        <div className='homepage'>
+            <Directory />
+        </div>
+    )
+}
